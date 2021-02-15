7849dbe359fd6438bc99d40136c9151b6e5bbfda
Joleana Shurley <joleanashurley@gmail.com>
2021-02-15 16:10:33 +0100

    generate controller for games; configure views and router paths

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,9 @@
+class GamesController < ApplicationController
+  def new
+    # TO DO: display a new random grid and a form
+  end
+
+  def score
+    # TO DO: takes form from new; submitted (with POST)
+  end
+end
